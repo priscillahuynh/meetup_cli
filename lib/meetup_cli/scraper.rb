c6597162576715a22ad2ec4b87d5502acae8625c
Priscilla H <priscillamhuynh@gmail.com>
2018-12-12 14:33:46 +0000

    removed whitespace from string elements

--- lib/meetup_cli/scraper.rb
+++ lib/meetup_cli/scraper.rb
@@ -2,8 +2,9 @@ class Scraper
   
   def self.scrape_meetups
     page = Nokogiri::HTML(open("https://www.meetup.com/find/tech/?allMeetups=false&radius=2&userFreeform=Los+Angeles%2C+California%2C+USA&mcId=c90001&change=yes&sort=distance&eventFilter=mysugg"))
-    heading = page.css('h3.padding-none').text.strip
-    heading_array = heading.split("\n\t\t\t\t\t\n")
+    heading = page.css('h3.padding-none').text.gsub("\t", "")
+    heading_array = heading.split("\n\n                        \n\n\n")
+    # heading_array.gsub { |text| text.gsub(/\n/,"")
     # .gsub(/\s+/, "")
     binding.pry
   end
