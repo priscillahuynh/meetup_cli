2a55f7ef9dc547dc87a73680b53058181d26c4b1
Priscilla H <priscillamhuynh@gmail.com>
2018-12-12 10:33:05 +0000

    added Nokogiri and Pry gems

--- Gemfile
+++ Gemfile
@@ -6,5 +6,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gemspec
 
 gem 'nokogiri'
-gem 'open-uri'
 gem 'pry'
--- lib/meetup_cli/scraper.rb
+++ lib/meetup_cli/scraper.rb
@@ -1,6 +1,7 @@
 class Scraper 
   
   def self.scrape_meetups
-    
+    page = Nokogiri::HTML(open("https://www.meetup.com/find/tech/?allMeetups=false&radius=2&userFreeform=Los+Angeles%2C+California%2C+USA&mcId=c90001&change=yes&sort=distance&eventFilter=mysugg"))
+    binding.pry
   end
 end
