99fa291225e6220e49fb49947cce6dcfa20da7bc
Priscilla H <priscillamhuynh@gmail.com>
2018-12-12 14:10:12 +0000

    created array of headers/titles for each meetup

--- lib/meetup_cli/cli.rb
+++ lib/meetup_cli/cli.rb
@@ -4,5 +4,6 @@ class CLI
     puts "Welcome to Tech Meetups in Los Angeles!"
     puts "Please select a Meetup from the list below to view details:"
     Scraper.scrape_meetups
+    #displays list of Meetup events for user to select from
   end
 end 
--- lib/meetup_cli/scraper.rb
+++ lib/meetup_cli/scraper.rb
@@ -2,6 +2,9 @@ class Scraper
   
   def self.scrape_meetups
     page = Nokogiri::HTML(open("https://www.meetup.com/find/tech/?allMeetups=false&radius=2&userFreeform=Los+Angeles%2C+California%2C+USA&mcId=c90001&change=yes&sort=distance&eventFilter=mysugg"))
-    # binding.pry
+    heading = page.css('h3.padding-none').text.strip
+    heading_array = heading.split("\n\t\t\t\t\t\n")
+    # .gsub(/\s+/, "")
+    binding.pry
   end
 end
