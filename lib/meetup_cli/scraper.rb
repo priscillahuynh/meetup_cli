aa15232f91b353f33c1c6bc431323e353eb0e294
Priscilla H <priscillamhuynh@gmail.com>
2018-12-13 05:02:05 +0000

    removed all whitespace characters from headings

--- lib/meetup_cli/scraper.rb
+++ lib/meetup_cli/scraper.rb
@@ -3,9 +3,9 @@ class Scraper
   def self.scrape_meetups
     page = Nokogiri::HTML(open("https://www.meetup.com/find/tech/?allMeetups=false&radius=2&userFreeform=Los+Angeles%2C+California%2C+USA&mcId=c90001&change=yes&sort=distance&eventFilter=mysugg"))
     heading = page.css('h3.padding-none').text.gsub("\t", "")
-    heading_array = heading.split("\n\n                        \n\n\n")
-    # heading_array.gsub { |text| text.gsub(/\n/,"")
-    # .gsub(/\s+/, "")
+    new_heading = heading.gsub("\n","")
+    heading_array = new_heading.split("                        ")
     binding.pry
+    # .gsub(/\s+/, "")
   end
 end
