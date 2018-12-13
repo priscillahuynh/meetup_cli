40d84c887cc8b07a61761fa12b7b3aa467211752
Priscilla H <priscillamhuynh@gmail.com>
2018-12-13 08:38:25 +0000

    set variables equal to name and members

--- lib/meetup_cli/scraper.rb
+++ lib/meetup_cli/scraper.rb
@@ -2,16 +2,17 @@ class Scraper
   
   def self.scrape_meetups
     page = Nokogiri::HTML(open("https://www.meetup.com/find/tech/?allMeetups=false&radius=2&userFreeform=Los+Angeles%2C+California%2C+USA&mcId=c90001&change=yes&sort=distance&eventFilter=mysugg"))
-    heading = page.css('h3.padding-none').text.gsub("\t", "")
-    new_heading = heading.gsub("\n","")
-    heading_array = new_heading.split("                        ")
-    heading_array.each.with_index(1) do |event, index|
-      puts "#{index}. #{event}"
+    heading_array = page.css('h3.padding-none').text.gsub("\t", "").gsub("\n","").split("                        ")
+    heading_array.each do |meetup|
+     name = meetup
     end
-    members = page.css('p.small.ellipsize').text.gsub("\t", "")
-    new_members = members.split("\n")
-    members_array = new_members.select {|n| n != """"}
-    binding.pry
+    members_array = page.css('p.small.ellipsize').text.gsub("\t", "").split("\n").select {|n| n != """"}
+    members_array.each do |m|
+      members = m
+      binding.pry 
+    end
+    
+    
   end
   
   
