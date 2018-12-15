95093388141e74827d7c420befe878448de9b366
Priscilla H <priscillamhuynh@gmail.com>
2018-12-15 11:55:10 +0000

    limit size of meetup array to 100

--- lib/meetup_cli/cli.rb
+++ lib/meetup_cli/cli.rb
@@ -3,42 +3,32 @@ class CLI
   def start 
     input = "menu"
     while input == "menu" 
-    puts "Welcome to Tech Meetups in Los Angeles!"
-    puts "Please select a Meetup from the list below to view details:"
-    Scraper.scrape_tech_meetups
-    meetups = Meetup.all
-    meetups.each.with_index(1) do |meetup, index|
-      puts "#{index}. #{meetup.name} with #{meetup.members}"
+      puts "Welcome to Tech Meetups in Los Angeles!"
+      puts "Please select a Meetup from the list below to view details:"
+      Scraper.scrape_tech_meetups
+      meetups = Meetup.all.take(100)
+      meetups.each.with_index(1) do |meetup, index|
+        puts "#{index}. #{meetup.name} with #{meetup.members}"
+      end
+      
+      input = gets.strip
+      index = input.to_i - 1 
+      selection = meetups[index]
+      
+      puts "You have selected #{selection.name}."
+      display_details(selection.url)
+      puts "For information on future meetup events, type 'events'. To go back to the main menu, type 'menu."
+      input = gets.strip
     end
-    
-    input = gets.strip
-    index = input.to_i - 1 
-    selection = meetups[index]
-    
-    puts "You have selected #{selection.name}."
-    display_details(selection.url)
-    puts "For information on future meetup events, type 'events'. To go back to the main menu, type 'menu."
-    menu_input = gets.strip
-      case menu_input
+      case input
         when "events"
           Scraper.scrape_meetup_event(selection.url)
           puts "To go back to the main menu, type 'menu', or to exit the program type 'exit'."
         when "menu"
           input = "menu"
+        
       end
-    end
-  end
-  
-  # input = gets.strip
-  
-  # if input == "menu"
-  #     start
-  # elsif input == "exit"
-  #     puts "Come back again soon!"
-  # else 
-  #     puts "Invalid input. Please try again."
-  # end
-  
+  end 
   
   def display_details(url)
     Scraper.scrape_meetup_page(url)
