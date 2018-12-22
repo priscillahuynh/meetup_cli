c5387430824a6d358883c3b295337775d428d4e2
Priscilla H <priscillamhuynh@gmail.com>
2018-12-22 10:27:14 +0000

    tweeked CLI

--- lib/meetup_cli/cli.rb
+++ lib/meetup_cli/cli.rb
@@ -1,45 +1,35 @@
 class CLI 
   
-  def start
+  def start 
     get_meetups
     input = "menu"
-    while input != "exit" 
-      puts "Welcome to Tech Meetups in Los Angeles!"
-      puts "Please select a Meetup from the list below to view details:"
-      @meetups.each.with_index(1) do |meetup, index|
-        puts "#{index}. #{meetup.name} with #{meetup.members}"
-      end
-      
+    while input == "menu" 
+      loop do 
+      select_meetup_and_display_details
+      puts "For information on future meetup events, type 'events'."
+      puts "To go back to the main menu, type 'menu."
+      puts "To exit the program, type 'exit'."
       input = gets.strip
-      index = input.to_i - 1 
-      selection = @meetups[index]
       
-      puts "You have selected #{selection.name}."
-      display_details(selection.url)
-      puts "For information on future meetup events, type 'events'." 
-      puts "To go back to the main menu, type 'menu'." 
-      puts "To leave the program at any time, type 'exit'."
-      input = gets.strip
-    end
-    
       case input
         when "events"
-          Scraper.scrape_meetup_event(selection.url)
+          Scraper.scrape_meetup_event(@selection.url)
           puts "To go back to the main menu, type 'menu', or to exit the program type 'exit'."
           final_input = gets.strip
           if final_input == "menu"
-            start
+            input = "menu"
           elsif final_input == 'exit'
             puts "Come back again soon!"
+              break
           end
         when "menu"
           input = "menu"
         when "exit"
           puts "Come back again soon!"
-        else
-          puts "Invalid input. Please try again."
-          input = "menu"
+            break
+        end
       end
+    end
   end 
   
   def display_details(url)
@@ -50,5 +40,19 @@ class CLI
     Scraper.scrape_tech_meetups
     @meetups = Meetup.all.take(100)
   end
+  
+  def select_meetup_and_display_details
+    puts "Welcome to Tech Meetups in Los Angeles!"
+    puts "Please select a Meetup from the list below to view details:"
+    @meetups.each.with_index(1) do |meetup, index|
+      puts "#{index}. #{meetup.name} with #{meetup.members}"
+    end
+    input = gets.strip
+    index = input.to_i - 1 
+    @selection = @meetups[index]
+    puts "You have selected #{@selection.name}."
+    display_details(@selection.url)
+  end 
+  
 end 
 
